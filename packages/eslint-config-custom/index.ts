9bdfd6b4ec3629789f4d892a1ab2f040114b67ab
gygamyt <gygamyt@gmail.com>
2025-08-25 16:47:21 +0200

    feat(core): linted and turbo config

--- packages/eslint-config-custom/index.ts
+++ packages/eslint-config-custom/index.ts
@@ -1,30 +1,30 @@
 const config = {
-  env: {
-    node: true,
-  },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
-  ],
-  plugins: ["@typescript-eslint"],
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: "latest",
-  },
-  settings: {
-    react: {
-      version: "detect",
+    env: {
+        node: true,
     },
-  },
-  rules: {
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-  },
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+    ],
+    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
 };
 
 module.exports = config; // <-- Ключевое исправление: используем module.exports
